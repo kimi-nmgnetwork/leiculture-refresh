563ff677d05ceeccd527731d01321201a62892cd
kimi-nmgnetwork <kimi@nmgnetwork.com>
2019-07-30 15:24:49 -1000

    fixed toggle

--- wp-content/themes/blogasm-child/js/scripts.js
+++ wp-content/themes/blogasm-child/js/scripts.js
@@ -10,4 +10,34 @@ document.addEventListener("DOMContentLoaded", () => {
       overlay.classList.remove("is-active");
     }
   });
+
+  // Create the img element for the nav link hover
+  const nav = document.querySelector("nav");
+  const hoverImgContainer = document.createElement("div");
+  hoverImgContainer.classList.add("nav-hover-img-container");
+  const hoverImg = document.createElement("img");
+  hoverImg.classList.add("hover-img");
+  hoverImgContainer.appendChild(hoverImg);
+  nav.appendChild(hoverImgContainer);
+
+  // CHANGE THE HOVER IMG TO THE LINK
+  const selectedImg = document.querySelector(".hover-img");
+  const pageItems = document.querySelectorAll(".page_item");
+
+  // Change this to img links later
+  const imgArr = ["red", "blue", "green", "orange", "purple", "yellow"];
+  let current;
+
+  pageItems.forEach((item, key) => {
+    // console.log(`${item.innerText}: ${key}`);
+    item.addEventListener("mouseover", () => {
+      current = key;
+
+      imgArr.map((img, key) => {
+        if (current == key) {
+          selectedImg.style.background = img;
+        }
+      });
+    });
+  });
 });
