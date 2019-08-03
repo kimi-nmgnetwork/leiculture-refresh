16c1a996997997f3e11aed1ff3722d0aa97ce11b
kimi-nmgnetwork <kimi@nmgnetwork.com>
2019-08-02 15:38:30 -1000

    refactored for mobile

--- wp-content/themes/blogasm-child/js/scripts.js
+++ wp-content/themes/blogasm-child/js/scripts.js
@@ -1,5 +1,3 @@
-console.log("updated");
-
 // Remove overlay if burger x is clicked
 document.addEventListener("DOMContentLoaded", () => {
   const burgerMenu = document.querySelector(".hamburger-menu");
@@ -19,6 +17,10 @@ document.addEventListener("DOMContentLoaded", () => {
   hoverImgContainer.classList.add("nav-hover-img-container");
   const hoverImg = document.createElement("img");
   hoverImg.classList.add("hover-img");
+  const hoverImgLink = document.createElement("a");
+  hoverImgLink.classList.add("hover-img-link");
+
+  hoverImgContainer.appendChild(hoverImgLink);
   hoverImgContainer.appendChild(hoverImg);
   nav.appendChild(hoverImgContainer);
 
@@ -28,25 +30,45 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Change this to img links later
   const imgArr = [
+    // MAKE SURE THE YOU FIX THE PATH FROM STAGING
     "http://staging.leiculture.com/wp-content/uploads/2019/08/arts_design.jpg",
     "http://staging.leiculture.com/wp-content/uploads/2019/08/culture.jpg",
     "http://staging.leiculture.com/wp-content/uploads/2019/08/fashion.jpg",
     "http://staging.leiculture.com/wp-content/uploads/2019/08/explore.jpg",
     "http://staging.leiculture.com/wp-content/uploads/2019/08/lei_tv.jpg",
-    "http://staging.leiculture.com/wp-content/uploads/2019/08/about.jpg"
+    "http://staging.leiculture.com/wp-content/uploads/2019/08/events.jpg",
+    "http://staging.leiculture.com/wp-content/uploads/2019/08/guides.jpg",
+    "http://staging.leiculture.com/wp-content/uploads/2019/08/subscribe.jpg"
   ];
   let current;
 
+  // SET DEFAULT HOVER IMG
+  selectedImg.src =
+    "http://staging.leiculture.com/wp-content/uploads/2019/08/arts_design.jpg";
+
   pageItems.forEach((item, key) => {
-    // console.log(`${item.innerText}: ${key}`);
+    let currentLink;
+
     item.addEventListener("mouseover", () => {
       current = key;
+      currentLink = item.querySelector("a").href;
 
       imgArr.map((img, key) => {
         if (current == key) {
           selectedImg.src = img;
+          hoverImgLink.href = currentLink;
+          console.log(hoverImgLink);
         }
       });
     });
   });
+
+  // USE THE OPENING ANIMATIONS
+  const heroImg = document.querySelector(".hero-img");
+  const scrollCta = document.querySelector(".scroll-cta");
+  const heroDescription = document.querySelector(".hero-description");
+
+  heroImg.classList.add("loaded");
+  scrollCta.classList.add("loaded");
+  heroDescription.classList.add("loaded");
 });
